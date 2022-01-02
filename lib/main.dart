4c1a0f9a1b780627083a5bf1f761df6e1ce4fad1
Luka-dot <79lukask@gmail.com>
2022-01-02 11:27:24 -0500

    basic basic set-up

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer selected!');
+  }
+
   Widget build(BuildContext context) {
+    var questionsList = [
+      'What is your favorite color?',
+      'What/s your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('New App 1'),
-          centerTitle: true,
-          bottomOpacity: 0.8,
-        ),
-        body: const Text(
-          'Tis is the main text of my App.',
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('New App '),
+            centerTitle: true,
+            bottomOpacity: 0.8,
+          ),
+          body: Column(
+            children: [
+              Text('Here will be the question!'),
+              ElevatedButton(
+                  child: Text(questionsList[0]), onPressed: answerQuestion),
+              RaisedButton(
+                child: Text(
+                  questionsList.elementAt(1),
+                ),
+                onPressed: () => print('Answer TWO choosen!'),
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer no:3 selected!');
+                },
+              ),
+            ],
+          )),
     );
   }
 }
